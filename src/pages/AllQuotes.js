de70346881f997d745f5ab117ab823dd512d09d6
cabonilla <christian.bonilla01@gmail.com>
2021-06-05 12:51:48 -0700

    Outputting DUMMY_DATA.

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,25 @@
+import QuoteList from '../components/quotes/QuoteList'
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Max',
+    text: 'I enjoy React!'
+  },
+  {
+    id: 'q2',
+    author: 'Mox',
+    text: 'I do not enjoy React.'
+  },
+  {
+    id: 'q3',
+    author: 'Mix',
+    text: 'I medium enjoy React?'
+  },
+]
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>
+  return <QuoteList quotes={DUMMY_QUOTES}/> 
 }
 
 export default AllQuotes
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,38 @@
 import { Fragment } from 'react'
 import { Route, useParams } from 'react-router-dom'
 import Comments from '../components/comments/Comments'
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
 
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Max',
+    text: 'I enjoy React!'
+  },
+  {
+    id: 'q2',
+    author: 'Mox',
+    text: 'I do not enjoy React.'
+  },
+  {
+    id: 'q3',
+    author: 'Mix',
+    text: 'I medium enjoy React?'
+  },
+]
 
 const QuoteDetail = () => {
   const params = useParams()
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author}/>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
