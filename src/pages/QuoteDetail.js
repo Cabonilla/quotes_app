325d75033684393ad74c7237058b0089e9a5d9af
cabonilla <christian.bonilla01@gmail.com>
2021-06-06 16:13:14 -0700

    Finished comments feature.

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,39 +1,42 @@
-import { Fragment } from 'react'
-import { Link, Route, useParams, useRouteMatch } from 'react-router-dom'
-import Comments from '../components/comments/Comments'
-import HighlightedQuote from '../components/quotes/HighlightedQuote'
-
-const DUMMY_QUOTES = [
-  {
-    id: 'q1',
-    author: 'Max',
-    text: 'I enjoy React!'
-  },
-  {
-    id: 'q2',
-    author: 'Mox',
-    text: 'I do not enjoy React.'
-  },
-  {
-    id: 'q3',
-    author: 'Mix',
-    text: 'I medium enjoy React?'
-  },
-]
+import { Fragment, useEffect } from 'react';
+import { Link, Route, useParams, useRouteMatch } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 
 const QuoteDetail = () => {
   const match = useRouteMatch()
   const params = useParams()
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+  const { quoteId } = params;
 
-  if (!quote) {
+  const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true)
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId])
+
+  if(status==='pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    )
+  }
+  
+  if (error) {
+    return <p className='centered'>{error}</p>
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className='centered'>
           <Link className='btn--flat' to={`${match.url}/comments`}>Load Comments</Link>
